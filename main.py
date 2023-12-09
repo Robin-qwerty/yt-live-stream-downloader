b098c027d0320f4b2f4006d82c661f0798213814
Robin-qwerty <robin@humilis.net>
2023-12-09 22:29:51 +0100

    upgraded code so less api calls will be made to the google api

--- main.py
+++ main.py
@@ -9,25 +9,17 @@ import sys
 BOLD = '\033[1m'
 RESET = '\033[0m'
 
-current_time = datetime.datetime.now()
-current_hour = current_time.hour
-
-# YouTube Data API Key
+# Read the API key from api-key.txt file
 with open('api-key.txt', 'r') as file:
     api_key = file.read().strip()  
-    
+
 # YouTube channel ID
 channel_id = 'UCWsDFcIhY2DBi3GB5uykGXA'
 
-# Array to store live stream video IDs
-live_stream_video_ids = []
-ids = ['UxoJYl5y4h8'] # test data
-live_stream_video_ids.extend(ids)
-print(live_stream_video_ids)
-print()
+# File to store live stream video IDs
+video_ids_file = 'live_stream_video_ids.txt'
 
 youtube = build('youtube', 'v3', developerKey=api_key)
-
 
 def get_live_stream_data(api_key, channel_id):
     response = youtube.search().list(
@@ -43,6 +35,18 @@ def get_live_stream_data(api_key, channel_id):
     else:
         return None
 
+def read_video_ids_from_file():
+    try:
+        with open(video_ids_file, 'r') as file:
+            lines = file.readlines()
+            return [line.strip() for line in lines if line.strip()]
+    except FileNotFoundError:
+        return []
+
+def write_video_id_to_file(video_id):
+    with open(video_ids_file, 'a') as file:
+        file.write(video_id + '\n')
+
 def create_folder(title):
     folder_path = f'/run/media/robin/big-partition/Videos/yt-dlp/speed-live-streams/{title}'
     subprocess.run(['mkdir', '-p', folder_path])
@@ -50,11 +54,12 @@ def create_folder(title):
 def execute_command(command):
     subprocess.Popen(["gnome-terminal", "--", "bash", "-c", command])
 
-
 while True:
     current_time = datetime.datetime.now()
+    current_hour = current_time.hour
     formatted_time = current_time.strftime("%H:%M:%S")
 
+    live_stream_video_ids = read_video_ids_from_file()
     try:
         live_stream_data = get_live_stream_data(api_key, channel_id)
 
@@ -64,19 +69,15 @@ while True:
             create_folder(live_stream_data)
             command = f"yt-dlp --live-from-start -P '/run/media/robin/big-partition/Videos/yt-dlp/speed-live-streams/{live_stream_data}' https://www.youtube.com/watch?v={live_stream_data}"
             execute_command(command)
-            # Add the video ID to the array
-            live_stream_video_ids.append(live_stream_data)
+            write_video_id_to_file(live_stream_data)  # Add the video ID to the file
         else:
             print(formatted_time, BOLD + '- No new live stream currently.' + RESET)
             
             print("<=========================================================>")
             try:
-                for video_id in live_stream_video_ids:
-                    # Retrieve video details using videos().list() API endpoint
-                    video_response = youtube.videos().list(
-                        part='snippet',
-                        id=video_id
-                    ).execute()
+                if live_stream_video_ids:
+                    video_id = live_stream_video_ids[-1]  # Get the last video ID from the file
+                    video_response = youtube.videos().list(part='snippet', id=video_id).execute()
 
                     if 'items' in video_response and len(video_response['items']) > 0:
                         print(f"Video with ID {BOLD}{video_id}{RESET}", Fore.GREEN + "Online", Style.RESET_ALL)
@@ -92,16 +93,16 @@ while True:
             print("<=========================================================>")
 
         # Check if the current time is between 7 PM and 2 AM
-        if current_hour > 20 or current_hour <2:
+        if current_hour > 20 or current_hour < 2:
             print("waiting 10 min before next check...")
             time.sleep(600)  # 10 minutes in seconds
         else:
             print("waiting 60 min before next check...")
-            time.sleep(3200)  # 60 minutes in seconds
+            time.sleep(3600)  # 60 minutes in seconds
 
     except Exception as e:
         print(BOLD + Fore.RED + "something went wrong" + RESET)
-        print(e)
+        print(formatted_time, "-", e)
 
         print("Trying again in 2 min")
         time.sleep(120)
